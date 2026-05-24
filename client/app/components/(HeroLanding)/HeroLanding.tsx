c65c7b3eedfc98031a3e3bc5bc08e81f2aeb6c66
Pencils0101001101001010 <sjuniversalpaint@gmail.com>
2026-05-24 13:40:54 +0200

    hero page style is done for now

--- client/app/components/(HeroLanding)/HeroLanding.tsx
+++ client/app/components/(HeroLanding)/HeroLanding.tsx
@@ -3,7 +3,15 @@ import "./heroStyles.css";
 export default function HeroLanding() {
   return (
     <div className="heroContainer">
-      <h1>Hello</h1>
+      <div className="borderTopLeft"></div>
+      <div className="borderTopLeft2"></div>
+      <span className="titleText">Welcome to everything</span>
+
+      <p className="textUnderTitle">
+        This will be a fun and learning experience{" "}
+      </p>
+      <div className="borderBottom"></div>
+      <div className="borderbttm2"></div>
     </div>
   );
 }
